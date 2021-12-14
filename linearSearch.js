adeeab9e53e44e004f7889e79fab85d7d5a5d697
Lameck1 <lameck72@gmail.com>
2021-12-14 22:42:10 +0300

    Try optimizing linearSearch method

--- linearSearch.js
+++ linearSearch.js
@@ -1,10 +1,19 @@
 const linearSearch = (arr, target) => {
+  let steps = 0;
+  if (arr[arr.length - 1] === target) return [arr.length - 1, 1];
   for (let value of arr) {
-    if (value === target) {
-      return arr.indexOf(value);
+    steps++;
+    if (value > target || arr[arr.length - 1] < target) {
+      break;
+    } else if (value === target) {
+      return [arr.indexOf(value), steps];
     }
   }
-  return -1;
+
+  return [-1, steps];
 };
 
-console.log(linearSearch([1, 2, 3, 4, 5], 3));
+console.log(linearSearch([1, 2, 3, 4, 5], 0));
+console.log(linearSearch([1, 2, 3, 4, 5], 10));
+console.log(linearSearch([1, 2, 3, 4, 5], 1));
+console.log(linearSearch([1, 2, 3, 4, 5], 5));
